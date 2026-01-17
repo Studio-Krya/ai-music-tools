b2c8bce1327ce4014a1c061595ea2092938062d4
Pablo Nascimento <pabloalexandre.nave@gmail.com>
2026-01-17 12:38:46 -0300

    feat: update startup

--- startup.sh
+++ startup.sh
@@ -2,5 +2,15 @@
 echo "Trying to update the project..."
 git pull
 
-clear
-# clear
+# check if the alias exists in the bashrc file even if the file doesnt exist
+has_command=$(grep -q '^alias krya=' "$HOME/.bashrc" 2>/dev/null && echo "found" || echo "not found")
+
+if [ "$has_command" == "not found" ]; then
+    echo "Alias krya not found, creating..."
+    echo "alias krya='uv run krya'" >> ~/.bashrc
+    
+    # should create other bashrc files in the home directory if they don't exist
+    if [ ! -f ~/.bash_profile ]; then
+        touch ~/.bash_profile
+    fi
+fi
