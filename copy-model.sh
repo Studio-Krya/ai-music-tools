923783c81e6f99dd4af8d0534c9e449eb2381711
Pablo Nascimento <pabloalexandre.nave@gmail.com>
2026-01-17 12:53:16 -0300

    add a helper script

---
+++ copy-model.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USER=$USERNAME
+FROM="/d/models/"
+TO="/c/Users/$USER/"
+
+echo "Pulling latest changes from git"
+git pull
+
+echo "Copying models from $FROM to $TO"
+
+# cp -r $FROM $TO
+
+# copying the shortcut to the desktop
+cp "./Krya AI Tools.lnk" "/c/Users/$USER/Desktop/Krya AI Tools.lnk"
