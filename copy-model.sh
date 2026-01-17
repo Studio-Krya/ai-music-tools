46d18dace0b78122f699be22b10c5ef3836c4a5f
Pablo Nascimento <pabloalexandre.nave@gmail.com>
2026-01-17 12:58:10 -0300

    fix script

--- copy-model.sh
+++ copy-model.sh
@@ -2,14 +2,14 @@
 
 USER=$USERNAME
 FROM="/d/models/"
-TO="/c/Users/$USER/"
+TO="/c/Users/$USER"
 
 echo "Pulling latest changes from git"
 git pull
 
 echo "Copying models from $FROM to $TO"
 
-# cp -r $FROM $TO
+cp -r $FROM $TO
 
 # copying the shortcut to the desktop
 cp "./Krya AI Tools.lnk" "/c/Users/$USER/Desktop/Krya AI Tools.lnk"
