430e4db7fb7e856a8fcac50768b6ad5c1aa2db0b
Pablo Salgado <pablo.salgado@varsitytutors.com>
2026-01-17 14:06:39 -0300

    helper script

--- copy-model.sh
+++ copy-model.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 USER=$USERNAME
-FROM="/d/models/."
-TO="/c/Users/$USER"
+
+TTS_DIR="/d/models/.local/share/tts/."
+CACHE_DIR="/d/models/.cache/."
+
+CACHE_DIR_OUT="/c/Users/$USER/.cache/"
+TTS_DIR_OUT="/c/Users/$USER/AppData/Local/tts"
 
 echo "Pulling latest changes from git"
 git pull
 
 echo "Copying models from $FROM to $TO"
 
-cp -r $FROM $TO
+cp -r $TTS_DIR $TTS_DIR_OUT
+cp -r $CACHE_DIR $CACHE_DIR_OUT
 
 # copying the shortcut to the desktop
 cp "./Krya AI Tools.lnk" "/c/Users/$USER/Desktop/Krya AI Tools.lnk"
